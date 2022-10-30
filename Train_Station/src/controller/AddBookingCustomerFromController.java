47060c9c5674b4e78e405fb290f59a5088b75bb7
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-30 23:23:01 +0530

    Add booking customer class

---
+++ Train_Station/src/controller/AddBookingCustomerFromController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+public class AddBookingCustomerFromController {
+    public Button btnBooking;
+    public TextField txtCusName;
+    public TextField txtCusId;
+    public TextField txtCusAddress;
+    public TextField txtCusContact;
+    public ComboBox cmbCusFrom;
+    public ComboBox cmbCusTo;
+    public ComboBox cmbCusTrain;
+    public TextField txtCusPrice;
+    public ComboBox cmbCusSeatNo;
+    public TextField txtTrainTime;
+    public ComboBox cmbCusClass;
+    public TableView tblCustomerBooking;
+    public TableColumn colCusId;
+    public TableColumn colCusName;
+    public TableColumn colCusAddress;
+    public TableColumn colCusTel;
+    public TableColumn colCusFrom;
+    public TableColumn colCusTo;
+    public TableColumn colCusTime;
+    public TableColumn colCusTrain;
+    public TableColumn colCusSeatNo;
+    public TableColumn colCusClass;
+    public TableColumn colCusPrice;
+    public TextField txtCusBookDate;
+    public TableColumn colDate;
+
+    public void btnBookingOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void textFields_Key_Releaseed(KeyEvent keyEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrintOnAction(ActionEvent actionEvent) {
+    }
+}
