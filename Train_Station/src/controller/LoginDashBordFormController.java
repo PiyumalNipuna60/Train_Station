4583f09e9eb38808f00c22ec06ca2da577fa5e9e
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-31 23:59:42 +0530

    Loging dash board class

---
+++ Train_Station/src/controller/LoginDashBordFormController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LoginDashBordFormController {
+    public Label lblRealTime;
+    public Label lblRealDate;
+    public Button btnSignIn;
+    public Button btnAdmin;
+    public Button btnUser;
+
+    public void btnUserLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAdminLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+    }
+}
