bc92962d25d69e061d31a02bb6be13a904066b6a
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-02 22:18:44 +0530

    Add Employeform class feilds

--- Train_Station/src/controller/AddEmployeeFromController.java
+++ Train_Station/src/controller/AddEmployeeFromController.java
@@ -1,4 +1,47 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class AddEmployeeFromController {
+    public Button btnAddEmployee;
+    public TextField txtEmpName;
+    public TextField txtEmpId;
+    public TextField txtEmpAddress;
+    public TextField txtEmpTel;
+    public TextField txtEmpAge;
+    public TextField txtEmpSalary;
+    public Button btnBack;
+    public AnchorPane employeeAnchorPane;
+    public TableView tblEmployee;
+    public TableColumn colEmpId;
+    public TableColumn colEmpName;
+    public TableColumn colEmpAddress;
+    public TableColumn colEmpAge;
+    public TableColumn colEmpTel;
+    public TableColumn colEmpSalary;
+
+
+    public void btnAddEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void textFields_Key_Releaseed(KeyEvent keyEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEmployeeReportOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnclearOnAction(ActionEvent actionEvent) {
+    }
 }
