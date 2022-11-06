f358a3effb0d6017e36df7bb19e9d436de60a00c
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-06 21:07:17 +0530

    AddTrainFrom : Create AddTrain Class

---
+++ Train_Station/src/controller/AddTrainFromController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+public class AddTrainFromController {
+
+    public ComboBox cmbTrainTo;
+    public ComboBox cmbTrainFrom;
+    public TextField txtTrainId;
+    public TextField txtTrainName;
+    public Button btnAddTrain;
+    public TextField txtStartTime;
+    public TextField txtEndTime;
+    public TableView tblAllTrain;
+    public TableColumn colTrainTo;
+    public TableColumn colTrainFrom;
+    public TableColumn colTrainId;
+    public TableColumn colTrainName;
+    public TableColumn colTrainStartTime;
+    public TableColumn colTrainEndTime;
+
+
+    public void btnAddTrainOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnclearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEmployeeReportOnAction(ActionEvent actionEvent) {
+    }
+
+    public void textFields_Key_Released(KeyEvent keyEvent) {
+    }
+}
