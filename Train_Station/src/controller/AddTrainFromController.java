d6028d25a82b6420e8a32f0a0cde52eed8a3af1f
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-07 19:12:59 +0530

    Addtrain form : Add Search method

--- Train_Station/src/controller/AddTrainFromController.java
+++ Train_Station/src/controller/AddTrainFromController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import model.Train;
 import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AddTrainFromController {
     public ComboBox cmbTrainTo;
@@ -29,7 +33,7 @@ public class AddTrainFromController {
         );
 
         try {
-            if (CrudUtil.executeUpdate("INSERT INTO train VALUES (?,?,?,?,?,?)"),t1.getTrainId(),t1.getTrainName(),t1.getStartTime(),t1.getEndTime(),t1.getTrainFrom(),t1.getTrainTo()){
+            if (CrudUtil.executeUpdate("INSERT INTO train VALUES (?,?,?,?,?,?)",t1.getTrainId(),t1.getTrainName(),t1.getStartTime(),t1.getEndTime(),t1.getTrainFrom(),t1.getTrainTo())){
                 new Alert(Alert.AlertType.CONFIRMATION,"Add Train!..").show();
             }else{
                 new Alert(Alert.AlertType.WARNING,"Something wrong!..").show();
@@ -40,6 +44,29 @@ public class AddTrainFromController {
     }
 
     public void txtSearchOnAction(ActionEvent actionEvent) {
+
+        try {
+            Train t1=searchTrain(txtTrainId.getText());
+        } catch (SQLException | ClassNotFoundException x) {
+            x.printStackTrace();
+        }
+
+    }
+
+    private Train searchTrain(String text) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM train WHERE trainId=?", txtTrainId.getText());
+        if (resultSet.next()){
+            return  new Train(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4),
+                    resultSet.getString(5),
+                    resultSet.getString(6)
+            );
+        }
+        return null;
+
     }
 
     public void btnclearOnAction(ActionEvent actionEvent) {
