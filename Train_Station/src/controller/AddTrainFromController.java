297287ed7c0f49a5bcda25c71aa9b93a4fb6e57f
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-07 19:39:36 +0530

    Addtrain form : Add comboTrainTo method

--- Train_Station/src/controller/AddTrainFromController.java
+++ Train_Station/src/controller/AddTrainFromController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -34,39 +33,50 @@ public class AddTrainFromController {
     }
 
     private void uploadComboBox() {
-        comboTrainFrom();
-        comboTrainTo();
+        try {
+            comboTrainFrom();
+            comboTrainTo();
+        } catch (SQLException | ClassNotFoundException x) {
+            x.printStackTrace();
+        }
+
 
     }
 
-    private void comboTrainFrom() {
-        try {
-            ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM station ORDER BY name ASC");
-            ObservableList obList = FXCollections.observableArrayList();
+    private void comboTrainTo() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM station ORDER BY name ASC");
+        ObservableList obList = FXCollections.observableArrayList();
 
-            while (resultSet.next()){
-                obList.add(new String(resultSet.getString(2)));
-            }
-
-        } catch (SQLException | ClassNotFoundException X) {
-            X.printStackTrace();
+        while (resultSet.next()) {
+            obList.add(new String(resultSet.getString(2)));
         }
+        cmbTrainTo.setItems(obList);
+    }
+
+    private void comboTrainFrom() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM station ORDER BY name ASC");
+        ObservableList obList = FXCollections.observableArrayList();
+
+        while (resultSet.next()) {
+            obList.add(new String(resultSet.getString(2)));
+        }
+        cmbTrainFrom.setItems(obList);
 
     }
 
 
     public void btnAddTrainOnAction(ActionEvent actionEvent) {
-        Train t1=new Train(
-                txtTrainId.getText(),txtTrainName.getText(),txtStartTime.getText(),txtEndTime.getText(),String.valueOf(cmbTrainFrom.getValue()),String.valueOf(cmbTrainTo.getValue())
+        Train t1 = new Train(
+                txtTrainId.getText(), txtTrainName.getText(), txtStartTime.getText(), txtEndTime.getText(), String.valueOf(cmbTrainFrom.getValue()), String.valueOf(cmbTrainTo.getValue())
         );
 
         try {
-            if (CrudUtil.executeUpdate("INSERT INTO train VALUES (?,?,?,?,?,?)",t1.getTrainId(),t1.getTrainName(),t1.getStartTime(),t1.getEndTime(),t1.getTrainFrom(),t1.getTrainTo())){
-                new Alert(Alert.AlertType.CONFIRMATION,"Add Train!..").show();
-            }else{
-                new Alert(Alert.AlertType.WARNING,"Something wrong!..").show();
+            if (CrudUtil.executeUpdate("INSERT INTO train VALUES (?,?,?,?,?,?)", t1.getTrainId(), t1.getTrainName(), t1.getStartTime(), t1.getEndTime(), t1.getTrainFrom(), t1.getTrainTo())) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Add Train!..").show();
+            } else {
+                new Alert(Alert.AlertType.WARNING, "Something wrong!..").show();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
@@ -74,16 +84,16 @@ public class AddTrainFromController {
     public void txtSearchOnAction(ActionEvent actionEvent) {
 
         try {
-            Train t1=searchTrain(txtTrainId.getText());
+            Train t1 = searchTrain(txtTrainId.getText());
 
-            if (t1!=null){
+            if (t1 != null) {
                 txtTrainId.setText(t1.getTrainId());
                 txtTrainName.setText(t1.getTrainName());
                 txtStartTime.setText(t1.getStartTime());
                 txtEndTime.setText(t1.getEndTime());
                 cmbTrainFrom.setValue(t1.getTrainFrom());
                 cmbTrainTo.setValue(t1.getTrainTo());
-            }else {
+            } else {
 
             }
 
@@ -95,8 +105,8 @@ public class AddTrainFromController {
 
     private Train searchTrain(String text) throws SQLException, ClassNotFoundException {
         ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM train WHERE trainId=?", txtTrainId.getText());
-        if (resultSet.next()){
-            return  new Train(
+        if (resultSet.next()) {
+            return new Train(
                     resultSet.getString(1),
                     resultSet.getString(2),
                     resultSet.getString(3),
@@ -120,7 +130,6 @@ public class AddTrainFromController {
 
     public void textFields_Key_Released(KeyEvent keyEvent) {
     }
-
 
 
     public void btnEmployeeReportOnAction(ActionEvent actionEvent) {
