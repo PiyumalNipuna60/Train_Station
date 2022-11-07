0875d684d7bd3943436e2d445d40d829f168976f
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-07 19:04:15 +0530

    Addtrain form : Add train

--- Train_Station/src/controller/AddTrainFromController.java
+++ Train_Station/src/controller/AddTrainFromController.java
@@ -3,6 +3,8 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import model.Train;
+import util.CrudUtil;
 
 public class AddTrainFromController {
     public ComboBox cmbTrainTo;
@@ -22,7 +24,19 @@ public class AddTrainFromController {
 
 
     public void btnAddTrainOnAction(ActionEvent actionEvent) {
+        Train t1=new Train(
+                txtTrainId.getText(),txtTrainName.getText(),txtStartTime.getText(),txtEndTime.getText(),String.valueOf(cmbTrainFrom.getValue()),String.valueOf(cmbTrainTo.getValue())
+        );
 
+        try {
+            if (CrudUtil.executeUpdate("INSERT INTO train VALUES (?,?,?,?,?,?)"),t1.getTrainId(),t1.getTrainName(),t1.getStartTime(),t1.getEndTime(),t1.getTrainFrom(),t1.getTrainTo()){
+                new Alert(Alert.AlertType.CONFIRMATION,"Add Train!..").show();
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Something wrong!..").show();
+            }
+        }catch (Exception e){
+
+        }
     }
 
     public void txtSearchOnAction(ActionEvent actionEvent) {
