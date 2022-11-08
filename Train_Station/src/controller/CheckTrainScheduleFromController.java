18e9bf337038a160b5f895e70043df4056d76507
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-08 19:08:52 +0530

    CheckTrainScheduleFrom : create crontroller class

---
+++ Train_Station/src/controller/CheckTrainScheduleFromController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class CheckTrainScheduleFromController {
+    public AnchorPane ScheduleAnchorPane;
+    public Button btnSearch;
+    public ComboBox cmbTrainTo;
+    public Button btnBack;
+    public TableColumn tblTrainTo;
+    public TableColumn tblTrainFrom;
+    public TableColumn tblTrainName;
+    public TableColumn tblTrainId;
+    public ComboBox cmbTrainFrom;
+    public TableView tblTrainLoad;
+    public TableColumn tblTrainStartTime;
+    public TableColumn tblTrainStopTime;
+    public TableColumn tblTrainStartStation;
+    public TableColumn tblTrainEndStation;
+    public TableColumn tblEndTime;
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrintOnAction(ActionEvent actionEvent) {
+    }
+}
