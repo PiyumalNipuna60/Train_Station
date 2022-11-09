432923233176c0937b686aadde13c2ede527996e
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-09 23:23:15 +0530

    LoginDashBordForm : load table data

--- Train_Station/src/AppInitialize.java
+++ Train_Station/src/AppInitialize.java
@@ -13,7 +13,7 @@ public class AppInitialize extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("views/EmployeeSalaryFrom.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("views/IncomeReportFrom.fxml"))));
         primaryStage.show();
     }
 }
--- Train_Station/src/controller/IncomeReportFromController.java
+++ Train_Station/src/controller/IncomeReportFromController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,7 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import model.BookingCustomer;
 import util.CrudUtil;
 
@@ -24,6 +28,17 @@ public class IncomeReportFromController {
     public TableView<BookingCustomer> tblReport;
 
     public void initialize() {
+
+        tblIncomeDate.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        tblIncome.setCellValueFactory(new PropertyValueFactory<>("price"));
+        tblOption.setCellValueFactory((param) -> {
+            ImageView edit = new javafx.scene.image.ImageView("/assets/draw.png");
+            ImageView delete = new ImageView("/assets/trash.png");
+            return new ReadOnlyObjectWrapper<>(new HBox(100, edit, delete));
+        });
+        
+
+
         try {
             loadTableData();
         } catch (SQLException | ClassNotFoundException x) {
