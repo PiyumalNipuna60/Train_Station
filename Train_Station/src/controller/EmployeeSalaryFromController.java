cf48065de2fc5574603f223319538cf0965041b9
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-09 22:48:54 +0530

    EmployeeSalaryFrom : add EmployeeSalaryFromController class

--- Train_Station/src/controller/EmployeeSalaryFromController.java
+++ Train_Station/src/controller/EmployeeSalaryFromController.java
@@ -1,10 +1,18 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import model.Employee;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class EmployeeSalaryFromController {
     public Button btnBack;
@@ -17,6 +25,40 @@ public class EmployeeSalaryFromController {
     public Button btnUpdate;
     public TableView tblEmployee;
     public AnchorPane salaryAnchorPane;
+
+    public void initialize() {
+
+        tblEmpId.setCellValueFactory(new PropertyValueFactory<>(""));
+        tblEmpName
+        tblEmpAddress
+        tblEmpAge
+        tblEmpTel
+        tblEmpSalary
+
+        try {
+            loadTableData();
+        } catch (SQLException | ClassNotFoundException x) {
+            x.printStackTrace();
+        }
+    }
+
+    private void loadTableData() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM employee");
+        ObservableList<Employee> obList= FXCollections.observableArrayList();
+        while (resultSet.next()){
+            obList.add(
+                    new Employee(
+                            resultSet.getString(1),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4),
+                            resultSet.getString(5),
+                            resultSet.getString(6)
+                    )
+            );
+        }
+        tblEmployee.setItems(obList);
+    }
 
 
     public void btnUpdateOnAction(ActionEvent actionEvent) {
