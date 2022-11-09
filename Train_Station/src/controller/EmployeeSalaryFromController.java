b0b4827f9dfc063956f4d5e552dffe3a7ef0ac34
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-09 22:39:46 +0530

    EmployeeSalaryFrom : add EmployeeSalaryFromController class

---
+++ Train_Station/src/controller/EmployeeSalaryFromController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class EmployeeSalaryFromController {
+    public Button btnBack;
+    public TableColumn tblEmpId;
+    public TableColumn tblEmpName;
+    public TableColumn tblEmpAddress;
+    public TableColumn tblEmpAge;
+    public TableColumn tblEmpTel;
+    public TableColumn tblEmpSalary;
+    public Button btnUpdate;
+    public TableView tblEmployee;
+    public AnchorPane salaryAnchorPane;
+
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrintOnAction(ActionEvent actionEvent) {
+    }
+}
