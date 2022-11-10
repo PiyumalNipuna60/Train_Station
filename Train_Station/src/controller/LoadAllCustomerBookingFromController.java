68bf0a5b8d92b36a339f26a550f5a6b3414d4f8a
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-10 22:53:44 +0530

    LoadAllCustomerBookingFrom : Add loadData method

--- Train_Station/src/controller/LoadAllCustomerBookingFromController.java
+++ Train_Station/src/controller/LoadAllCustomerBookingFromController.java
@@ -1,10 +1,17 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import model.BookingCustomer;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoadAllCustomerBookingFromController {
 
@@ -25,11 +32,36 @@ public class LoadAllCustomerBookingFromController {
     public TableColumn colDate;
 
     public void initialize() {
-        loadData();
+        try {
+            loadData();
+        } catch (SQLException | ClassNotFoundException x) {
+            x.printStackTrace();
+        }
     }
 
-    private void loadData() {
+    private void loadData() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM booking");
+        ObservableList<> obList= FXCollections.observableArrayList();
+        while (resultSet.next()){
+            obList.add(
+                    new BookingCustomer(
+                            resultSet.getString(1),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4),
+                            resultSet.getString(5),
+                            resultSet.getString(6),
+                            resultSet.getString(7),
+                            resultSet.getString(8),
+                            resultSet.getString(9),
+                            resultSet.getString(10),
+                            resultSet.getString(11),
+                            resultSet.getString(12)
+                    )
+            );
 
+        }
+        tblCustomerBooking.setItems(obList);
     }
 
     public void btnBackOnAction(ActionEvent actionEvent) {
