4622b4bc551b062dc0d084919eed0acaa31da2bf
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-11 23:26:53 +0530

    LoadAllEmployeeFrom : Create load All Employee method

--- Train_Station/src/controller/LoadAllEmployeeFromController.java
+++ Train_Station/src/controller/LoadAllEmployeeFromController.java
@@ -10,6 +10,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import model.Employee;
+import util.CrudUtil;
 
 
 import java.io.IOException;
@@ -29,8 +31,30 @@ public class LoadAllEmployeeFromController {
 
     public void initialize() {
 
+
+        try {
+            loadAllEmployee();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void loadAllEmployee() throws ClassNotFoundException, SQLException {
+        ResultSet result = CrudUtil.executeQuery("SELECT * FROM employee");
+        ObservableList<Employee> obList = FXCollections.observableArrayList();
+        while (result.next()) {
+            obList.add(
+                    new Employee(
+                            result.getString("id"),
+                            result.getString("name"),
+                            result.getString("address"),
+                            result.getString("age"),
+                            result.getString("contact"),
+                            result.getString("salary")
+                    ));
+        }
+        tblEmployee.setItems(obList);
+    }
 
 
     public void btnUpdateOnAction(ActionEvent actionEvent) throws IOException {
