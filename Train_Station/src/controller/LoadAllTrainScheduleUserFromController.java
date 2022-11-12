b12936dc43ae0d492420aaf9f94939032daa5bc6
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-12 13:12:43 +0530

    LoadAllTrainScheduleUserFrom : Create LoadAllTrainScheduleUser class

---
+++ Train_Station/src/controller/LoadAllTrainScheduleUserFromController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class LoadAllTrainScheduleUserFromController {
+
+    public TableColumn colTrainTo;
+    public TableColumn colTrainFrom;
+    public TableColumn colTrainId;
+    public TableColumn colTrainName;
+    public TableColumn colTrainStartTime;
+    public TableColumn colTrainEndTime;
+    public TableView tblAllTrain;
+    public AnchorPane AllEmployeeAnchorPane;
+
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrintOnAction(ActionEvent actionEvent) {
+
+    }
+}
