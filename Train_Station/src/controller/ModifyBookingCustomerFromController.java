4bfdccceb5c3c1008c8e38f6775b5c0aa9879e3a
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-14 18:59:14 +0530

    ModifyBookingCustomerFrom : create ModifyBookingCustomerFromController class

---
+++ Train_Station/src/controller/ModifyBookingCustomerFromController.java
@@ -0,0 +1,55 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+public class ModifyBookingCustomerFromController {
+
+    public Button btnModifyBooking;
+    public TextField txtCusName;
+    public TextField txtCusId;
+    public TextField txtCusAddress;
+    public TextField txtCusContact;
+    public ComboBox cmbCusFrom;
+    public ComboBox cmbCusTo;
+    public ComboBox cmbCusTrain;
+    public TextField txtCusPrice;
+    public ComboBox cmbCusSeatNo;
+    public Button btnDeleteBooking;
+    public TextField txtTrainTime;
+    public ComboBox cmbCusClass;
+    public TableView tblCustomerBooking;
+    public TableColumn colCusId;
+    public TableColumn colCusName;
+    public TableColumn colCusAddress;
+    public TableColumn colCusFrom;
+    public TableColumn colCusTel;
+    public TableColumn colCusTo;
+    public TableColumn colCusTime;
+    public TableColumn colCusTrain;
+    public TableColumn colCusSeatNo;
+    public TableColumn colCusClass;
+    public TableColumn colCusPrice;
+    public TableColumn colCusDate;
+    public TextField txtDate;
+
+
+    public void btnModifyBookingOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteBookingOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrintOnAction(ActionEvent actionEvent) {
+    }
+
+    public void textFields_Key_Releaseed(KeyEvent keyEvent) {
+    }
+}
