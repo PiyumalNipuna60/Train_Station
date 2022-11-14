092ee1c35312134aa73503521b03d89f9ec59fda
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-14 23:19:21 +0530

    ModifyBookingFrom : txtSearchOnAction method add

--- Train_Station/src/controller/ModifyBookingCustomerFromController.java
+++ Train_Station/src/controller/ModifyBookingCustomerFromController.java
@@ -1,8 +1,15 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import model.BookingCustomer;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ModifyBookingCustomerFromController {
 
@@ -39,6 +46,50 @@ public class ModifyBookingCustomerFromController {
     }
 
     public void txtSearchOnAction(ActionEvent actionEvent) {
+        try {
+            BookingCustomer b=searchingBooking(txtCusId.getText());
+            if (b!=null){
+                txtCusId.setText(b.getId());
+                txtCusName.setText(b.getName());
+                txtCusAddress.setText(b.getAddress());
+                txtCusContact.setText(b.getContact());
+                cmbCusFrom.setValue(b.getTrainFrom());
+                cmbCusTo.setValue(b.getTrainTo());
+                txtTrainTime.setText(b.getTime());
+                cmbCusTrain.setValue(b.getTrain());
+                cmbCusSeatNo.setValue(b.getSeatNo());
+                cmbCusClass.setValue(b.getTrainClass());
+                txtCusPrice.setText(b.getPrice());
+                txtDate.setText(b.getDate());
+            }else {
+                new Alert(Alert.AlertType.WARNING, "Empty values!..").show();
+            }
+        } catch (SQLException | ClassNotFoundException x) {
+            x.printStackTrace();
+        }
+    }
+
+    private BookingCustomer searchingBooking(String text) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.executeQuery("SELECT * FROM booking WHERE id=?", text);
+
+
+        if (resultSet.next()){
+                   return new BookingCustomer(
+                           resultSet.getString(1),
+                           resultSet.getString(2),
+                           resultSet.getString(3),
+                           resultSet.getString(4),
+                           resultSet.getString(5),
+                           resultSet.getString(6),
+                           resultSet.getString(7),
+                           resultSet.getString(8),
+                           resultSet.getString(9),
+                           resultSet.getString(10),
+                           resultSet.getString(11),
+                           resultSet.getString(12)
+            );
+        }
+        return null;
     }
 
     public void btnDeleteBookingOnAction(ActionEvent actionEvent) {
