afa45e7e3d461530bd7ed405317a0925aa79a553
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-21 22:10:35 +0530

    ModifyTrainFrom : Add ModifyTrainFromController class

---
+++ Train_Station/src/controller/ModifyTrainFromController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ModifyTrainFromController {
+
+    public Button btnRemoveTrain1;
+    public Button btnBack;
+    public ComboBox cmbTrainTo;
+    public ComboBox cmbTrainFrom;
+    public TextField txtTrainId;
+    public TextField txtTrainName;
+    public Button btnUpdateTrain;
+    public TextField txtStartTime;
+    public TextField txtEndTime;
+    public AnchorPane trainAnchorPane;
+    public TableView tblAllTrain;
+    public TableColumn colTrainTo;
+    public TableColumn colTrainFrom;
+    public TableColumn colTrainId;
+    public TableColumn colTrainName;
+    public TableColumn colTrainStartTime;
+    public TableColumn colTrainEndTime;
+
+
+
+    public void btnUpdateTrainOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRemoveTrainOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnclearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void textFields_Key_Releaseed(KeyEvent keyEvent) {
+    }
+
+
+
+    public void btnEmployeeReportOnAction(ActionEvent actionEvent) {
+    }
+}
