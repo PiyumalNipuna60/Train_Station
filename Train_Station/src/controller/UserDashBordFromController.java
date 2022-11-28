1b8abb16dd83174b78ea94e7685841c25cd7c4aa
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-11-28 23:35:26 +0530

    Add UserDashboard classs

---
+++ Train_Station/src/controller/UserDashBordFromController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class UserDashBordFromController {
+
+    public Button btnBookingCustomer;
+    public Button btnModifyCustomer;
+    public Button btnLoadAllCustomer;
+    public Button btnLoadSchedule;
+    public Button btnMaintenance;
+    public Button btnCheckSchedule;
+    public Button btnLogOut;
+    public Button btnCancel;
+    public Label lblRealTime;
+    public Label lblRealDate;
+    public AnchorPane mainAnchorPane;
+    public AnchorPane secondAnchorPane;
+
+
+    public void btnBookingCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModifyCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoadAllCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoadScheduleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMaintenanceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCheckScheduleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+}
