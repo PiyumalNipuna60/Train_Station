14fbee3aec3a055627e71bc9b5de8d6e8681b5b9
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-05 23:45:28 +0530

    LoginDashBordFormController : generateRealTime

--- Train_Station/src/controller/LoginDashBordFormController.java
+++ Train_Station/src/controller/LoginDashBordFormController.java
@@ -1,8 +1,16 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LoginDashBordFormController {
     public Label lblRealTime;
@@ -11,12 +19,29 @@ public class LoginDashBordFormController {
     public Button btnAdmin;
     public Button btnUser;
 
+    public void initialize(){
+        generateRealTime();
+    }
+
     public void btnUserLoginOnAction(ActionEvent actionEvent) {
+
     }
 
     public void btnAdminLoginOnAction(ActionEvent actionEvent) {
+
     }
 
     public void btnSignInOnAction(ActionEvent actionEvent) {
+
+    }
+
+    private void generateRealTime() {
+        lblRealDate.setText(LocalDate.now().toString());
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+            lblRealTime.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 }
