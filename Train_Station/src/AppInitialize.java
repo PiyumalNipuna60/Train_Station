c54ba6072f1eef6114df8d550c9707a1b67566c6
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-06 22:44:28 +0530

    LoginAdminFrom : create LoginAdminFrom

--- Train_Station/src/AppInitialize.java
+++ Train_Station/src/AppInitialize.java
@@ -13,7 +13,7 @@ public class AppInitialize extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("views/ModifyTrainFrom.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("views/LoginDashBordForm.fxml"))));
         primaryStage.show();
     }
 }
